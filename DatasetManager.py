377e3bd1037a84a3fead4b8bcf1100f11a557ea7
alan-lira <alan_lira@id.uff.br>
2021-06-05 15:55:10 -0300

    Added data splitting step (to be used on ML model).

--- DatasetManager.py
+++ DatasetManager.py
@@ -1,4 +1,5 @@
 import pandas
+import numpy
 from sklearn.preprocessing import MinMaxScaler
 
 class DatasetManager:
@@ -31,3 +32,24 @@ class DatasetManager:
 
    def dataset_column_null_values_count(self, dataset, dataset_column):
       return len(dataset.index) - dataset[[dataset_column]].count()[0]
+
+   def split_normalized_train_and_test_data_chunks(self, normalized_column, trainning_percent):
+      normalized_train_data_chunk = []
+      normalized_test_data_chunk = []
+      trainning_chunk_size = int(len(normalized_column) * trainning_percent)
+      testing_chunk_size = len(normalized_column) - trainning_chunk_size
+      for train_index in range(0, trainning_chunk_size):
+         normalized_train_data_chunk.append(normalized_column[train_index])
+      for test_index in range(trainning_chunk_size, len(normalized_column)):
+         normalized_test_data_chunk.append(normalized_column[test_index])
+      normalized_train_data_chunk = numpy.reshape(normalized_train_data_chunk, (trainning_chunk_size, 1))
+      normalized_test_data_chunk = numpy.reshape(normalized_test_data_chunk, (testing_chunk_size, 1))
+      return normalized_train_data_chunk, normalized_test_data_chunk
+
+   def split_normalized_past_and_future_data_chunks(self, normalized_column_chunk, start_index, end_index, past_size, future_size):
+      normalized_past_data_chunk = []
+      normalized_future_data_chunk = []
+      for index in range(start_index + past_size, end_index):
+         normalized_past_data_chunk.append(numpy.reshape(normalized_column_chunk[range(index - past_size, index)], (past_size, 1)))
+         normalized_future_data_chunk.append(normalized_column_chunk[index + future_size])
+      return normalized_past_data_chunk, normalized_future_data_chunk
--- main.py
+++ main.py
@@ -30,3 +30,21 @@ normalized_close_column = datasetManager.normalize_dataset_values_of_column(data
 
 ## PRINT ORIGINAL AND NORMALIZED VALUES OF 'Close' COLUMN (CLOSE PRICE)
 datasetManager.print_original_and_normalized_dataset_column(dataset, 'Close', normalized_close_column)
+
+## SPLIT NORMALIZED TRAIN AND TEST DATA CHUNKS (TRAINNING DATA PERCENT: 80%)
+trainning_data_percent = 0.8
+normalized_train_data_chunk, normalized_test_data_chunk = datasetManager.split_normalized_train_and_test_data_chunks(normalized_close_column, trainning_data_percent)
+
+## SET AMOUNT OF DAYS TO LEARN TO PREDICT THE FUTURE TIME SERIES
+past_size = 5 # LEARN 5 DAYS
+future_size = 0 # PREDICT THE NEXT DAY
+
+## SPLIT NORMALIZED PAST AND FUTURE TRAIN DATA CHUNKS
+train_start_index = 0
+train_end_index = len(normalized_train_data_chunk)
+normalized_past_train_data_chunk, normalized_future_train_data_chunk = datasetManager.split_normalized_past_and_future_data_chunks(normalized_train_data_chunk, train_start_index, train_end_index, past_size, future_size)
+
+## SPLIT NORMALIZED PAST AND FUTURE TEST DATA CHUNKS
+test_start_index = 0
+test_end_index = len(normalized_test_data_chunk)
+normalized_past_test_data_chunk, normalized_future_test_data_chunk = datasetManager.split_normalized_past_and_future_data_chunks(normalized_test_data_chunk, test_start_index, test_end_index, past_size, future_size)
